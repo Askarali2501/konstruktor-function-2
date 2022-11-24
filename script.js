ce4218b15aa0a8f094edc9f3eefe22300b74db15
Oscar <exaple@gmail.com>
2022-11-24 10:07:52 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// const biography = {
+//     name:`Askarali`,
+//     lastname:`Makhmudov`,
+//     age:13, 
+//     func:function(){
+//         return this.age 
+//     }
+// }
+// console.log(biography.func());
+// function User()
+function Car (color, name,) {
+    this.color = color;
+    this.name = name;
+}
+
+const mercedes = new Car('black', 'Mercedes');
+const bmw = new Car('white', 'BMW')
+console.log(mercedes);
+console.log(bmw);
